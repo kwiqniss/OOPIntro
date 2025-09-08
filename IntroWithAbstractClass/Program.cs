d3752a50988ec11764c3b19eef0aab56d6309331
David Hollowell <davidhollowell@live.com>
2025-09-08 02:32:19 -0400

    Update to show a second instance of student.

--- IntroWithAbstractClass/Program.cs
+++ IntroWithAbstractClass/Program.cs
@@ -11,21 +11,48 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
         {
             Console.WriteLine("Hello " + Person.Genus + " " + Person.Species);
 
-            Student student = new Student("Jane", "Doe", 170, GradeLevel.Freshman, "JD");
-            Instructor instructor = new Instructor("Robert", "Smith", 185, "Bob");
+            Student student = new Student("Jane", "Doe", 170, GradeLevel.Sophomore, "JD");
+            var student2 = new Student("Samuel", "Winchester", 193, GradeLevel.Freshman, "Sammy");
+            //var students = new Student[] { student, student2 }; // this would work to create our student array, but it would be unusual to declare the student and student2 variables and immediately put them in an array since that's creating variables that are only used once and it's on the next line, so there's no reason for the overhead of creatin a variable. See sample below.
+
+            Instructor instructor = new Instructor("Robert", "Singer", 185, "Bobby");
 
             instructor.Speak();
             student.Listen("");
+            student2.Listen("");
 
             student.Speak();
+            student2.Speak();
             instructor.Listen("");
 
             student.NickName = "Janey";
             student.Speak();
 
             student.PrepareToReceiveInstruction();
+            student2.PrepareToReceiveInstruction();
             instructor.Instruct(Message);
             student.ReceiveInstruction(Message);
+            student2.ReceiveInstruction(Message);
+
+            // a better way to do that would be to have a collection of students, and loop through them instead of being so redundant.
+            var students = new Student[]
+            {
+                new Student("Jane", "Doe", 170, GradeLevel.Sophomore, "JD"),
+                new Student("Samuel", "Winchester", 193, GradeLevel.Freshman, "Sammy")
+            };
+
+            foreach (var s in students)
+            {
+                s.PrepareToReceiveInstruction();
+            }
+
+            instructor.Instruct(Message);
+
+            foreach (var s in students)
+            {
+                s.ReceiveInstruction(Message);
+            }
+
 
             Console.WriteLine();
             Console.Write("press any key to exit...");
