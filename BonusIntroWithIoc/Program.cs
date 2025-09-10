b7e76c5ddf876fbeb1bd6acf1408b387bf4d91ff
David Hollowell <davidhollowell@live.com>
2025-09-09 21:30:17 -0400

    comments

--- BonusIntroWithIoc/Program.cs
+++ BonusIntroWithIoc/Program.cs
@@ -113,14 +113,14 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
                 // let's see if the user entered 1, v1, V1, 2, v2, or V2. If they did, return the appropriate enum value. Otherwise, return Unknown.
 
                 if (input == 
-                    ((int)VersionToCall.V1).ToString() // this is similar to input == "1", but if we ever change the enum value, this will automatically update too. We're casting the enum to it's base value of an int. Then we call ToString() to convert the int to a string for comparison.
+                    ((int)VersionToCall.V1).ToString() // this is similar to input == "1", but if we ever change the enum value, this will automatically update too. We're casting the enum to its base value of an int. Then we call ToString() to convert the int to a string for comparison since input from the console is always a string.
                     || string.Equals(input, VersionToCall.V1.ToString(), StringComparison.OrdinalIgnoreCase)) // similar to string1 == string2, except we're ignoring any potential differences in casing (upper/lower)
                 {
                     return VersionToCall.V1;
                 }
                 else if (input ==
-                    ((int)VersionToCall.V2).ToString()
-                    || string.Equals(input, VersionToCall.V2.ToString(), StringComparison.OrdinalIgnoreCase))
+                    ((int)VersionToCall.V2).ToString() // this part compares the string when the user typed an integer only; we grab the backing int value of the enum and convert it to a string for comparison since input from the console is always a string.
+                    || string.Equals(input, VersionToCall.V2.ToString(), StringComparison.OrdinalIgnoreCase)) // this part compares the string when the user types 'v' before the version number. We're comparing to the string representation of the enum value and ignoring differences in casing.
                 {
                     return VersionToCall.V2;
                 }
